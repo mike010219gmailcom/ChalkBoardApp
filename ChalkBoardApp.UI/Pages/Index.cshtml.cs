a02ca88d0099f050b84735641a827b5afe3cf456
Nando040 <nando.salvatierra@outlook.com>
2026-02-11 11:57:46 +0100

    Skapade loginsidan/index/startsidan inte logiken än, nästa mål är att skapa register, member och admin sidan.

--- ChalkBoardApp.UI/Pages/Index.cshtml.cs
+++ ChalkBoardApp.UI/Pages/Index.cshtml.cs
@@ -1,13 +1,47 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace ChalkBoardApp.UI.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [BindProperty, Required]
+        public string Username { get; set; }
+        [BindProperty, Required]
+        public string Password { get; set; }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //Anropa signInManager för att logga in användaren
+            //PasswordSignIn kollar användare + lösenord, och skapar en cookie
+            var result = await _signInManager.PasswordSignInAsync(Username, Password, false, false);
+
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(Username);
+                if (await _userManager.IsInRoleAsync(user, "Admin"))
+                {
+                    return RedirectToPage("");
+                }
+                return RedirectToPage("");
+            }
+
+            return Page();
         }
     }
 }
